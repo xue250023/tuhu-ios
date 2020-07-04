d42859aa845888b6a9da7e230851c8fb561275fa
tuhuadmin <tuhuadmin@hcMacbookPro.local>
2020-07-04 18:02:26 +0800

    v1

--- TuhuiOSLessonDemoApp/ViewController.swift
+++ TuhuiOSLessonDemoApp/ViewController.swift
@@ -10,41 +10,74 @@ import UIKit
 //
 import Alamofire
 import  SwiftyJSON
-class ViewController: UIViewController  {
+class ViewController: UIViewController ,SliderGalleryControllerDelegate {
+
 
     var comments: JSON?
     var goodsInfo: JSON?
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var popview: UIView!
-   
-    
-    override func viewDidLoad() {
+   //--- 轮播图----/
+    //获取屏幕宽度
+    let screenWidth =  UIScreen.main.bounds.size.width
+
+    //图片轮播组件
+    var sliderGallery : SliderGalleryController!
+
+    //图片集合
+    var images = ["https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1593665767162&di=c1020f64d786a325a67ee879e3ac9e5d&imgtype=0&src=http%3A%2F%2Fimg.app178.com%2Ftu%2F201410%2Fyhrgcry32t4.jpg",
+        "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1593665767162&di=d006cd9610c46120b1aabe234ab4812c&imgtype=0&src=http%3A%2F%2Fattach.bbs.miui.com%2Fforum%2F201401%2F22%2F134002dhye9qe2g2nq2geu.jpg",
+                  "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1593665767161&di=3725ac75d78e2823033bb54bdf33e3ea&imgtype=0&src=http%3A%2F%2F01.minipic.eastday.com%2F20170330%2F20170330044723_a0c69f758cc90e87e8c8e620eb55308e_2.jpeg",
+                  "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1593665767160&di=73123c4ec1a7def8d911d815147d385c&imgtype=0&src=http%3A%2F%2Fattach.bbs.miui.com%2Fforum%2F201312%2F03%2F165620x7cknad7vruvec1z.jpg",
+                  "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1593665767159&di=1bf25e664df31768dc4010fabb44d4e2&imgtype=0&src=http%3A%2F%2Fc.hiphotos.baidu.com%2Fzhidao%2Fpic%2Fitem%2F54fbb2fb43166d22d552b941432309f79052d23a.jpg"]
+
+        //--轮播图--/
+        override func viewDidLoad() {
         super.viewDidLoad()
         //MARK: set Datasource
         self.tableView.dataSource = self
         self.tableView.delegate = self
         popview.isHidden = true
-//        GetProductDetail();
-//        SelectProductTopNComments();
-       
+        //--轮播图--/
+        //初始化图片轮播组件
+        sliderGallery = SliderGalleryController()
+        sliderGallery.delegate = self
+        sliderGallery.view.frame = CGRect(x: 10, y: 40, width: screenWidth,
+                                          height: (screenWidth-20)/4*3);
+        //将图片轮播组件添加到当前视图
+        //self.addChild(sliderGallery)
+        //tableView.tableHeaderView =  sliderGallery.view
+        //---轮播图--/
+        
+        
+        
+        
+        
+        
+       AF.request("https://api.tuhu.cn/Product/GetProductDetail?activityId=&pid=OL-MO-ONE-5W40-1%7C").response { response in
+               
+          let data = JSON.init(response.data)
+          self.goodsInfo = data
+          let coms = self.goodsInfo? ["Comments"]
+          let images=data["Product"]["Image"]["ImageUrls"]
+          let imagesStrArr: [String] = images.arrayValue.map({ $0.stringValue })
+          print("----goods11------")
+          print(imagesStrArr)
+          print("----goods11------")
+        self.tableView.reloadData()
+        
+
+       }
         AF.request("https://api.tuhu.cn/Comment/SelectProductTopNComments?productId=OL-MO-ONE-5W40-1").response { response in
                 
             let data = JSON.init(response.data)
             self.comments = data;
-            print (self.comments)
+            
+            
         }
         
         
-        AF.request("https://api.tuhu.cn/Product/GetProductDetail?activityId=&pid=OL-MO-ONE-5W40-1%7C").response { response in
-                
-            let data = JSON.init(response.data)
-            self.goodsInfo = data
-            let coms = self.goodsInfo?["Comments"]
-            print("----goods------")
-            print(coms?[0])
-             print("----goods------")
 
-        }
         
     }
 
@@ -62,6 +95,17 @@ class ViewController: UIViewController  {
       override func didReceiveMemoryWarning() {
           super.didReceiveMemoryWarning()
       }
+    
+    //--lunbotu--/
+    func galleryScrollerViewSize() -> CGSize {
+        return CGSize(width: screenWidth-20, height: (screenWidth-20)/4*3)
+    }
+
+    //图片轮播组件协议方法：获取数据集合
+    func galleryDataSource() -> [String] {
+        return images
+    }
+
 
     
 }
@@ -100,11 +144,16 @@ extension ViewController:UITableViewDataSource{
         let  cell:UITableViewCell=tableView.dequeueReusableCell(withIdentifier: identifer, for: indexPath)
         
         
+         let coms = self.comments?["Comments"]
+        
         
         if let imagecell : ImageCell=cell as? ImageCell{
             //设置图片
         }else if let priceCell:PriceCell=cell as? PriceCell{
-            priceCell.nowPriceLabel.text="$100"
+            priceCell.nowPriceLabel.text = goodsInfo?["Product"]["Price"].stringValue;
+            print("----1111---")
+            print(goodsInfo?["Product"]["Price"])
+              print("---1111----")
             //设置价格
         }else if let titleCell:TitleCell=cell as? TitleCell{
             //设置title
@@ -239,3 +288,68 @@ class CommentCell : UITableViewCell{
  */
 
 
+//class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegate, SliderGalleryControllerDelegate {
+//    @IBOutlet weak var tableView: UITableView!
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 1;
+//    }
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//         let identifer:[String] = [
+//                    "imageCell"
+//                ]
+//                let id = identifer[indexPath.item]
+//                let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: id, for: indexPath)
+//                return cell
+//    }
+//    //Mark:设置高度
+//     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        switch indexPath.item {
+//        case 0 :
+//            return 360
+//        default:
+//            return 0
+//        }
+//    }
+//
+//    //获取屏幕宽度
+//    let screenWidth =  UIScreen.main.bounds.size.width
+//
+//    //图片轮播组件
+//    var sliderGallery : SliderGalleryController!
+//
+//    //图片集合
+//    var images = ["https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1593665767162&di=c1020f64d786a325a67ee879e3ac9e5d&imgtype=0&src=http%3A%2F%2Fimg.app178.com%2Ftu%2F201410%2Fyhrgcry32t4.jpg",
+//        "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1593665767162&di=d006cd9610c46120b1aabe234ab4812c&imgtype=0&src=http%3A%2F%2Fattach.bbs.miui.com%2Fforum%2F201401%2F22%2F134002dhye9qe2g2nq2geu.jpg",
+//                  "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1593665767161&di=3725ac75d78e2823033bb54bdf33e3ea&imgtype=0&src=http%3A%2F%2F01.minipic.eastday.com%2F20170330%2F20170330044723_a0c69f758cc90e87e8c8e620eb55308e_2.jpeg",
+//                  "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1593665767160&di=73123c4ec1a7def8d911d815147d385c&imgtype=0&src=http%3A%2F%2Fattach.bbs.miui.com%2Fforum%2F201312%2F03%2F165620x7cknad7vruvec1z.jpg",
+//                  "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1593665767159&di=1bf25e664df31768dc4010fabb44d4e2&imgtype=0&src=http%3A%2F%2Fc.hiphotos.baidu.com%2Fzhidao%2Fpic%2Fitem%2F54fbb2fb43166d22d552b941432309f79052d23a.jpg"]
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        //MARK: set Datasource
+//        self.tableView.dataSource = self
+//        self.tableView.delegate = self
+//
+//        //初始化图片轮播组件
+//        sliderGallery = SliderGalleryController()
+//        sliderGallery.delegate = self
+//        sliderGallery.view.frame = CGRect(x: 10, y: 40, width: screenWidth,
+//                                          height: (screenWidth-20)/4*3);
+//        //将图片轮播组件添加到当前视图
+//        self.addChild(sliderGallery)
+//        tableView.tableHeaderView =  sliderGallery.view
+//    }
+//
+//    //图片轮播组件协议方法：获取内部scrollView尺寸
+//    func galleryScrollerViewSize() -> CGSize {
+//        return CGSize(width: screenWidth-20, height: (screenWidth-20)/4*3)
+//    }
+//
+//    //图片轮播组件协议方法：获取数据集合
+//    func galleryDataSource() -> [String] {
+//        return images
+//    }
+//
+//
+//
+//}
